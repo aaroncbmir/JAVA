e98b730b06a1bf80f80eefa35098b3b97fa821be
aaroncabrera <contactoaaroncb@gmail.com>
2018-09-10 19:48:59 +0200

    New

--- app/src/main/java/com/example/aaroncabrera/myapplication/Principal.java
+++ app/src/main/java/com/example/aaroncabrera/myapplication/Principal.java
@@ -11,6 +11,7 @@ public class Principal {
 
 Funcion f = new Seno();
 Funcion g = new Logaritmo();
+Polinomio p = new Polinomio();
 System.out.println(f.calcular(1.0));
 System.out.println(g.calcular(5.0));
 
@@ -19,12 +20,16 @@ System.out.println(g.calcular(5.0));
                 new ArrayList<Funcion>();
         fun.add(new Seno());
         fun.add(new Logaritmo());
+        fun.add(p);
+        p.añadir(2);
+        p.añadir(5);
+        p.añadir(8);
+        p.añadir(3);
 
         for (int i = 0; i < fun.size(); i++) {
             Funcion t = fun.get(i);
             System.out.println(t.calcular(1.0));
         }
-
     }
 }
 
