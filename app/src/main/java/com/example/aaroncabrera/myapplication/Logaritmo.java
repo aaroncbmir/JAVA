fcd429a5ebd24829a95a921c1c25e2753400773c
aaroncabrera <contactoaaroncb@gmail.com>
2018-09-10 18:09:18 +0200

    New

---
+++ app/src/main/java/com/example/aaroncabrera/myapplication/Logaritmo.java
@@ -0,0 +1,11 @@
+package com.example.aaroncabrera.myapplication;
+
+public class Logaritmo implements Funcion {
+
+    public double calcular (double x) {
+        return Math.log(x);
+    }
+
+
+
+}
--- app/src/main/java/com/example/aaroncabrera/myapplication/Principal.java
+++ app/src/main/java/com/example/aaroncabrera/myapplication/Principal.java
@@ -1,5 +1,7 @@
 package com.example.aaroncabrera.myapplication;
 
+import java.util.ArrayList;
+
 public class Principal {
 
 
@@ -8,7 +10,20 @@ public class Principal {
     {
 
 Funcion f = new Seno();
+Funcion g = new Logaritmo();
 System.out.println(f.calcular(1.0));
+System.out.println(g.calcular(5.0));
+
+
+        ArrayList<Funcion> fun =
+                new ArrayList<Funcion>();
+        fun.add(new Seno());
+        fun.add(new Logaritmo());
+
+        for (int i = 0; i < fun.size(); i++) {
+            Funcion t = fun.get(i);
+            System.out.println(t.calcular(1.0));
+        }
 
     }
 }
